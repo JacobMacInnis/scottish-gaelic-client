03ef446b5da22effd25556b297b8141d2eb513bf
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-12-04 11:07:10 -0500

    added demo-account component to show user credentials

--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './about.css';
+import DemoAccount from './demo-account';
 
 export default function About(props){
     return(
         <div className='about-container'>
-            <p className='about-content'>Scottish Gaelic is a language learning app designed to improve your abilty to learn the beautiful Scottish language of Gaelic.  Using our specialized algorithm we will teach you words and phrases that will build your language skills and based on your abilities you will progress quickly and with a stronger grasp of Gaelic!<br/><br/>Create
-            your account today to speak the traditional Scottish language.</p>
+            <p className='about-content'>Scottish Gaelic is a language learning app designed to improve your abilty to learn the beautiful Scottish language of Gaelic.  Using our specialized algorithm we will teach you words and phrases that will build your language skills and based on your abilities you will progress quickly and with a stronger grasp of Gaelic!</p>
+            <DemoAccount />
         </div>
     )
 }
